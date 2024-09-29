983a655b8b5091a6e3da220ae7e1f85d2e1f30fe
root <root@Kerry>
2024-09-29 22:33:53 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  function superbowlWin(record) {
+    // Use find() to look for the object with result "W"
+    const win = record.find(game => game.result === "W");
+  
+    // If a win is found, return the year; otherwise, return undefined
+    return win ? win.year : undefined;
+  }
